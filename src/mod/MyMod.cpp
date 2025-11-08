9567383f5e3035197a1ae9e9f1ea64521d312e58
L QJ <96654617+2PuceWriter79890@users.noreply.github.com>
2025-11-08 18:18:21 +0800

    Add files via upload

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,29 +1,55 @@
 #include "mod/MyMod.h"
 
 #include "ll/api/mod/RegisterHelper.h"
+#include "ll/api/io/Logger.h"
+#include "ll/api/memory/Hook.h"
+#include "mc/world/actor/Mob.h"
+#include "mc/world/actor/Actor.h"
+#include "mc/deps/core/string/HashedString.h"
+
+namespace {
+
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    CheckSpawnRulesHook,
+    ll::memory::HookPriority::Normal,
+    Mob,
+    
+    &Mob::$checkSpawnRules,
+    
+    bool,
+    bool checkSpawnPosition
+) {
+    if (this->hasFamily(HashedString("zombie"))) {
+        return origin(checkSpawnPosition); //
+    }
+
+    return false;
+}
+
+}
 
 namespace my_mod {
 
-MyMod& MyMod::getInstance() {
+MyMod& MyMod::getInstance() { //
     static MyMod instance;
     return instance;
 }
 
-bool MyMod::load() {
-    getSelf().getLogger().debug("Loading...");
-    // Code for loading the mod goes here.
+bool MyMod::load() { //
+    getSelf().getLogger().info("加载 AllowOnlyZombieSpawning 中...");
+    getSelf().getLogger().info("Author: @2PuceWriter79890 - Github RE-Studio-CN");
     return true;
 }
 
-bool MyMod::enable() {
-    getSelf().getLogger().debug("Enabling...");
-    // Code for enabling the mod goes here.
+bool MyMod::enable() { //
+    getSelf().getLogger().info("启用 AllowOnlyZombieSpawning 中...");
+    getSelf().getLogger().info("Hook 成功");
     return true;
 }
 
-bool MyMod::disable() {
-    getSelf().getLogger().debug("Disabling...");
-    // Code for disabling the mod goes here.
+bool MyMod::disable() { //
+    getSelf().getLogger().info("禁用 AllowOnlyZombieSpawning 中...");
+    getSelf().getLogger().info("Hook 已卸载");
     return true;
 }
 
--- src/mod/MyMod.h
+++ src/mod/MyMod.h
@@ -1,33 +1,29 @@
 #pragma once
 
-#include "ll/api/mod/NativeMod.h"
+#include "ll/api/mod/NativeMod.h" //
 
 namespace my_mod {
 
 class MyMod {
 
 public:
-    static MyMod& getInstance();
+    static MyMod& getInstance(); //
 
-    MyMod() : mSelf(*ll::mod::NativeMod::current()) {}
+    MyMod() : mSelf(*ll::mod::NativeMod::current()) {} //
 
-    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; }
+    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; } //
 
     /// @return True if the mod is loaded successfully.
-    bool load();
+    bool load(); //
 
     /// @return True if the mod is enabled successfully.
-    bool enable();
+    bool enable(); //
 
     /// @return True if the mod is disabled successfully.
-    bool disable();
-
-    // TODO: Implement this method if you need to unload the mod.
-    // /// @return True if the mod is unloaded successfully.
-    // bool unload();
+    bool disable(); //
 
 private:
-    ll::mod::NativeMod& mSelf;
+    ll::mod::NativeMod& mSelf; //
 };
 
 } // namespace my_mod
